ab41690e12a2f790ca689c27843789139feb4290
Mohammad Reza Karimi <m.r.karimi.j@gmail.com>
2026-05-14 17:50:06 -0400

    add more startup stuff

--- run.sh
+++ run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-turnstile-update-runit-env WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE </dev/null
+turnstile-update-runit-env WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
 
-/usr/libexec/xdg-desktop-portal </dev/null &
-/usr/libexec/xdg-desktop-portal-wlr </dev/null &
-/usr/libexec/polkit-mate-authentication-agent-1 </dev/null &
-mako </dev/null &
-waybar </dev/null &
-wbg -s /home/moreka/.config/wallpaper </dev/null &
-# /home/moreka/.config/niri/scripts/swayidle.sh </dev/null &
-
-rm -f /tmp/dwl-waybar.status.eDP-1
-rm -f /tmp/dwl-waybar.status.HDMI-A-1
-
-/home/moreka/builds/dwl-waybar/dwlway >/tmp/logggg
+/usr/libexec/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal-wlr &
+/usr/libexec/polkit-mate-authentication-agent-1 &
+mako &
+waybar &
+wbg -s /home/moreka/.config/wallpaper &
+swayidle -w timeout 601 'wlr-dpms off' timeout 600 'swaylock -f' before-sleep 'swaylock -f' &
+wl-clip-persist --clipboard regular &
 
 wait
