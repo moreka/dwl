6936a406cb0dd5a08eef76e258bfc9cff5d548e5
Mohammad Reza Karimi <m.r.karimi.j@gmail.com>
2026-06-19 12:43:24 -0400

    update for archlinux

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-turnstile-update-runit-env WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE
+systemctl --user import-environment WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE \
+  PATH EDITOR SUDO_EDITOR XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME XDG_STATE_HOME XDG_BIN_HOME \
+  CARGO_HOME TEXMFHOME TEXMFVAR TEXMFCONFIG GOPATH GTK2_RC_FILES PASSWORD_STORE_DIR NOTMUCH_CONFIG \
+  XCURSOR_THEME SSH_AUTH_SOCK
 
-/usr/libexec/xdg-desktop-portal &
-/usr/libexec/xdg-desktop-portal-wlr &
-/usr/libexec/polkit-mate-authentication-agent-1 &
+dbus-update-activation-environment --all
+
+/usr/lib/xdg-desktop-portal &
+
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+
 mako &
-waybar &
+# waybar &
 wbg -s /home/moreka/.config/wallpaper &
 swayidle -w timeout 600 'swaylock -f' timeout 601 'wlr-dpms off' resume 'wlr-dpms on' before-sleep 'swaylock -f' &
 wl-clip-persist --clipboard regular &
-# foot --server &
+
+# somebar &
+# someblocks &
 
 wait
